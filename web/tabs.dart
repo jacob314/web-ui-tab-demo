7d44d78f6495e67f4b3ca7000e60d5731b9fcd7b
Jacob Richman <jacob314@gmail.com>
2013-04-03 13:47:17 -0700

    Checked mode fix

--- web/tabs.dart
+++ web/tabs.dart
@@ -6,13 +6,18 @@ import 'pane.dart';
 
 @observable
 class Tabs extends WebComponent {
-  var panes = new ObservableList<Pane>();  
+  var panes = new ObservableList<Pane>();
 
-  void select(Pane pane) => panes.forEach((p) => p.selected = p == pane);
-  void removePane(Pane pane) => panes.remove(pane);
+  void select(Pane pane) {
+    panes.forEach((p) => p.selected = p == pane);
+  }
+
+  void removePane(Pane pane) {
+    panes.remove(pane);
+  }
 
   void addPane(Pane pane) {
     panes.add(pane);
     if (panes.length == 1) select(pane);
-  } 
+  }
 }
